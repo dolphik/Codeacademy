00d1eda10ae092703e4fbfc71c9145e66ba0eaee
dolphik <nick.birkus@gmail.com>
2021-06-02 00:31:25 -0700

    nothing

---
+++ HTML&CSS Projects/hello_html/file1.js
@@ -0,0 +1,5 @@
+            alert("Hello world!");
+            let div = document.querySelector("div");
+            div.addEventListener("click", function() {
+                div.innerText = "Hi Bro";
+            })
